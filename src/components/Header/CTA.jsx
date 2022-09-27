d8c0e626096808cbc87daa014cb6aa99e40ee217
Marvyn97 <marvynpaulin@gmail.com>
2022-09-27 19:50:14 +0200

    update CV

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import CV from '../../assets/Marvyn_Paulin_CV.pdf';
 
 const CTA = () => {
   return (
     <div className='CTA'>
-        <a href='CV' download className='btn'>télécharger CV</a>
+        <a href={CV} download className='btn'>télécharger CV</a>
         <a href='#contact' className='btn btn-primary'>Me contacter</a>
     </div>
   )
